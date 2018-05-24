57606eb039963d7c451e4cb3c5cc81c88f17a774
Andrii Vasyliev <sol@hiqdev.com>
2018-05-24 16:21:43 +0000

    inited empty tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Control panel for hosting, domains and more.
+ *
+ * @link      https://hipanel.com/
+ * @package   hipanel
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2018, HiQDev (http://hiqdev.com/)
+ */
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/../vendor/autoload.php';
